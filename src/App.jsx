edd71ac491f5109ecb4933e6c73346cccb1c85f5
ttodawg01 <tonycampos1803@gmail.com>
2022-11-13 22:39:20 -0600

    needs to be updated

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Home from './components/Home';
+
+function App() {
+  return (
+  <>
+    <Home />
+    <div id="container">
+      </div>
+  </>
+  );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,21 @@
+import React, {useEffect, useState} from 'react'
+
+export default function Home(props) {
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        fetch("https://kekambas-blog.herokuapp.com//blog/posts")
+        .then(response => response.json())
+        .then(result => {
+            console.log(result)
+            setPosts(result)
+        })
+        .catch(error => console.log('error', error));
+    }, [])
+    return (
+    <>
+    <h1 className='text-center'>Home Page</h1>
+    {posts.map(x => <p>{x.content}</p>)}
+    </>
+  )
+}
